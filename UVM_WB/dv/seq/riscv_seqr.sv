62fb0529165152ced619fdc327b74e5e4ec4a36a
makararasi <87912209+makararasi@users.noreply.github.com>
2021-09-12 19:56:41 +0530

    Add files via upload

---
+++ UVM_WB/dv/seq/riscv_seqr.sv
@@ -0,0 +1,1 @@
+typedef uvm_sequencer #(riscv_seq_item) riscv_seqr;
---
+++ UVM_WB/dv/seq/seq_pkg.sv
@@ -0,0 +1,10 @@
+
+
+package seq_pkg;
+    import uvm_pkg::*;
+    `include "uvm_macros.svh"
+    `include "riscv_seqr.sv"
+    `include "riscv_seq_item.svh"
+    `include "sequence_base.svh"
+
+endpackage
